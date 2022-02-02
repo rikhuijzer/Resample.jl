b89db009ac0d6c0a33f52724aa67a38c07bc6f59
Rik Huijzer <rikhuijzer@pm.me>
2022-02-02 13:18:36 +0100

    Add tests for 2D smote

--- src/smote.jl
+++ src/smote.jl
@@ -1,7 +1,7 @@
 "Default number of nearest neighbors for SMOTE"
 const DEFAULT_K = 5
 
-_npoints(data) = size(data, 2)
+_npoints(data::AbstractMatrix) = size(data, 2)
 
 """
 Return one new point between the points A and B.
@@ -14,16 +14,31 @@ function _new_point(A::AbstractVector, B::AbstractVector)
     return A .+ new_dist
 end
 
+_point(data::AbstractMatrix, index) = data[:, index]
+
+"""
+Return a random neighbor from the `k` nearest neighbors for `random_point`.
+Since `NearestNeighbors.knn` also works for finding the nearest neighbor for points that are not in `data`, it will also return `random_point` as a nearest neighbor.
+So, we call `knn(..., k + 1)` and throw away the first result.
+We could avoid this by creating a new tree for each search, but that is more expensive.
+"""
+function _random_neighbor(tree, data, random_point, k)
+    sortres = true
+    idxs, _ = knn(tree, random_point, k + 1, true)
+    popfirst!(idxs)
+    random_neighbor_index = rand(idxs)
+    random_neighbor = _point(data, random_neighbor_index)
+    return random_neighbor
+end
+
 """
 Return one new point for some random point in `data`.
 """
 function _new_point(rng::AbstractRNG, data::AbstractVecOrMat, tree, k)
     n_minority = _npoints(data)
     random_point_index = rand(1:n_minority)
-    random_point = data[:, random_point_index]
-    idxs, _ = knn(tree, random_point, k)
-    random_neighbor_index = rand(idxs)
-    random_neighbor = data[:, random_neighbor_index]
+    random_point = _point(data, random_point_index)
+    random_neighbor = _random_neighbor(tree, data, random_point, k)
     new_point = _new_point(random_point, random_neighbor)
     return new_point
 end
@@ -36,9 +51,9 @@ _detect_k(data)::Int = min(DEFAULT_K, _npoints(data) - 1)
 
 Return the sample obtained via Synthetic Minority Over-sampling TEchnique (SMOTE) (Chawla et al., [2002](https://doi.org/10.1613/jair.953)) for
 
-- `data`: data where each column denotes a point
-- `n`: number of synthetic points that should be created
-- `k`: number of nearest neighbors to consider for each point
+- `data`: Data where each column denotes a point.
+- `n`: Number of synthetic points that should be created.
+- `k`: Number of nearest neighbors to consider for each point.
 
 For each minority class, the algorithm creates synthetic points along the lines in between the `k` nearest neighbors.
 The location of the point along the line is chosen randomly.
--- test/smote.jl
+++ test/smote.jl
@@ -1,12 +1,24 @@
 rng = StableRNG(1)
 
 # 2D points in some minority class.
-data = [1.0 2.0 3.0;
-        4.0 5.0 6.0]
+p1_x = 0.0
+p1_y = 2.0
+p2_x = 1.0
+p2_y = 3.0
 
-@test Resample._detect_k(data) == 2
+data = [p1_x p2_x;
+        p1_y p2_y]
+
+@test Resample._detect_k(data) == 1
 
 n = 2
 new_points = smote(data, n)
 @test Resample._npoints(new_points) == n
 
+for point in eachcol(new_points)
+    x = point[1]
+    @test p1_x ≤ x ≤ p2_x
+
+    y = point[2]
+    @test p2_x ≤ y ≤ p2_y
+end
